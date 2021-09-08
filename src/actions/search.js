88d99c930cb9451962866ed00f264465aba627fd
Kevin Diaz <kevindi0695@gmail.com>
2021-09-08 13:41:45 -0300

    main 11

--- src/actions/search.js
+++ src/actions/search.js
@@ -48,13 +48,7 @@ export const searchDetailsId = (id)=>{
 
             method: 'GET',
             url: `https://github.com/Rob--W/cors-anywhere/https://superheroapi.com/api/10226000777461921/${id}`,
-            mode: 'no-cors',           
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'origin':'x-requested-with',
-                'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
-                'Content-Type': 'application/json',
-            }
+           
 
         }
 
